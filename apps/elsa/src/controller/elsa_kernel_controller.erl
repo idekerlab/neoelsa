3a8009ccabc95e09c24b0364ec5619056cdd1202
Eric Sage <eric.david.sage@gmail.com>
2016-04-13 22:11:36 +0000

    Draft finished

--- apps/elsa/src/controller/elsa_kernel_controller.erl
+++ apps/elsa/src/controller/elsa_kernel_controller.erl
@@ -1,91 +1,34 @@
 
 -module(elsa_kernel_controller).
 
--export([parse/1
-       , new_task/1
-       , resource_exists/1
-       , resource_missing/2
-       , timeout/1
-       , connect/3
-       , create_task/1
-       , create_response/2]).
+-export([resource_exists/2
+       , resource_missing/3
+       , find_service/2
+       , return_thread/3]).
 
--record(kernel, {method, name, version, endpoint, headers, body, timeout}).
-
-parse(Req) ->
-  {N, Req1} =  cowboy_req:binding(service_name, Req),
-  {V, Req2} = cowboy_req:binding(service_version, Req1),
-  {E, Req3} = cowboy_req:path(Req2),
-  {T, Req4} = cowboy_req:header(<<"x-elsa-timeout">>, Req3, <<"5000">>),
-  {H, Req5} = cowboy_req:headers(Req4),
-  {M, Req6} = cowboy_req:method(Req5),
-  {ok, B, Request} = cowboy_req:body(Req6, [{length, infinity}]),
-  {Request, #kernel{method=M, name=N, version=V, endpoint=truncate(N, V, E), headers=H, body=B, timeout=T}}.
-
-new_task(#kernel{name=N, version=V}) ->
-  elsa_task_controller:new(elsa_hash:sha(N, V)).
-
-resource_exists(#kernel{name=N, version=V}) ->
+resource_exists(N, V) ->
   case elsa_service_controller:all(N, V) of
     {false, _} -> false;
     {true, [Service]} -> elsa_service:instance_count(Service) > 0
   end.
 
-timeout(#kernel{timeout=T}) when is_integer(T) -> T;
-timeout(#kernel{timeout=T}) when is_binary(T) -> binary_to_integer(T).
-
-create_task(#kernel{name=N, version=V}) ->
-  elsa_task_controller:new(elsa_hash:sha(N, V)).
-
-connect(Handler, K = #kernel{name=N, version=V, method=M, endpoint=E, headers=H, body=B}, Task) ->
-  {Loc, InstanceID, ThreadID} = find_thread(N, V),
-  elsa_task_controller:set_resource(Task, InstanceID, ThreadID),
-  elsa_task_controller:update_status(Task, connected),
-  lager:info("Calling m: ~p, url: ~p, h: ~p, b: ~b", [M, url(Loc, E), H, B]),
-  case elsa_http_client:call(M, url(Loc, E), H, B) of
-    {ok, Status, Headers, Body} ->
-      elsa_thread_controller:put(N, V, InstanceID, ThreadID),
-      lager:info("Connection succeeded"),
-      respond(Status, Headers, Body, Task, Handler);
-    retry ->
-      lager:error("Could not connect"),
-      elsa_task_controller:update_status(Task, retrying_with_new_connection),
-      connect(Handler, K, Task)
-  end.
-
-create_response({Status, Headers, Body}, Res) ->
-  {ok, Response} = cowboy_req:reply(Status, Headers, Body, Res),
-  Response;
-create_response(Task, Res) ->
-  {ok, Response} = cowboy_req:reply(200, [], elsa_body:to_json(Task), Res),
-  Response.
-
-find_thread(N, V) ->
+find_service(N, V) ->
   case elsa_thread_controller:get(N, V) of
-    none_available -> find_thread(N, V);
-    Thread -> Thread
+    none_available -> find_service(N, V);
+    {Loc, InstanceID, ThreadID} = Thread ->
+      {split_url(Loc), Thread}
   end.
 
-respond(S, H, B, Task, Handler) ->
-  lager:info("Response: ~p, ~p, ~p", [S, H, B]),
-  receive
-    timeout ->
-      Task
-    after 0 ->
-     Handler ! {S, H, B}
-  end.
+return_thread(N, V, {_, Ir, Tr}) ->
+  elsa_thread_controller:put(N, V, Ir, Tr).
 
-url(L, E) -> iolist_to_binary([L, E]).
-
-
-truncate(V, N, E) ->
-  VL = length(binary_to_list(V)),
-  NL = length(binary_to_list(N)),
-  list_to_binary(string:sub_string(binary_to_list(E), (3 + VL + NL))).
-
-resource_missing(#kernel{name=N, version=V}, Request) ->
+resource_missing(N, V, Request) ->
   Req = elsa_body:write(Request, [
    {<<"status">>, 404},
    {<<"service_name">>, N},
    {<<"service_version">>, V}
   ]), cowboy_req:reply(404, Req).
+
+split_url(Loc) ->
+  [Url, Port] = binary:split(Loc, <<":">>),
+  {binary_to_list(Url), binary_to_integer(Port)}.
--- apps/elsa/src/handler/elsa_kernel_handler_v2.erl
+++ apps/elsa/src/handler/elsa_kernel_handler_v2.erl
@@ -5,30 +5,65 @@
 
 start() ->
   {ok, Listen} = gen_tcp:listen(8080, [binary, {packet, line}, {reuseaddr, true}, {active, false}]),
-  lager:info("Socket opened"),
   accept(Listen).
 
 accept(Listen) ->
-  {ok, Socket} = gen_tcp:accept(Listen),
-  lager:info("Connection made"),
-  spawn(fun() -> handle_conn(Socket, Listen) end),
+  {ok, Client} = gen_tcp:accept(Listen),
+  spawn(fun() -> handle_conn(Client) end),
   accept(Listen).
 
-handle_conn(Socket, Listen) ->
-  case gen_tcp:recv(Socket, 0) of
-    {ok, Bin} ->
-      lager:info("Server said: ~p", [Bin]),
-      parse(Bin);
-    {error, closed} ->
-      lager:info("Server socket closed~n")
-  end.
+handle_conn(Client) ->
+  {Service, Name, Version, Thread} = parse_request_line(Client),
+  ContentLength = parse_headers(Client, Service, 0),
+  parse_body(Client, Service, ContentLength, 0),
+  ContentLength2 = parse_headers(Service, Client, 0),
+  parse_body(Service, Client, ContentLength2, 0),
+  elsa_kernel_controller:return_thread(Name, Version, Thread).
 
+parse_request_line(Client) ->
+  {ok, Line} = gen_tcp:recv(Client, 0),
+  {Name, Version} = parse(Line),
+  {{Url, Port}, Thread} = elsa_kernel_controller:find_service(Name, Version),
+  {ok, Service} = gen_tcp:connect(Url, Port, [binary, {packet, line}]),
+  gen_tcp:send(Client, Line),
+  {Service, Name, Version, Thread}.
+
+parse_headers(Client, Service, ContentLength) ->
+  case gen_tcp:recv(Client, 0) of
+    {ok, Line = <<"\r\n">>} ->
+      gen_tcp:send(Service, Line),
+      inet:setopts(Client, [{packet, raw}]);
+    {ok, Line = <<"Content-Length: ", N/binary>>} ->
+      gen_tcp:send(Service, Line),
+      parse_headers(Client, Service, content_length(N));
+    {ok, Line} ->
+      gen_tcp:send(Service, Line),
+      parse_headers(Client, Service, ContentLength);
+    {error, closed} ->
+      lager:error("Connection dropped")
+  end,
+  ContentLength.
+
+parse_body(Client, Service, ContentLength, ReadSoFar) ->
+ if ReadSoFar == ContentLength ->
+      gen_tcp:close(Client);
+    true ->
+      case gen_tcp:recv(Client, 0, 100) of
+        {ok, Bin} ->
+          gen_tcp:send(Service, Bin),
+          parse_body(Client, Service, ContentLength, ReadSoFar + size(Bin));
+        {error, closed} ->
+          lager:info("Connection dropped")
+      end
+ end.
+
+content_length(Val) ->
+  [Num, _] = binary:split(Val, <<"\r">>),
+  binary_to_integer(Num).
 
 parse(RequestLine) ->
   [Verb, Rest] = binary:split(RequestLine, <<" ">>),
-  [_, Service, Version | Rest2] = binary:split(Rest, <<"/">>, [global]),
+  [_, Name, Version | Rest2] = binary:split(Rest, <<"/">>, [global]),
   URL = [Verb,<<" /">>,Rest2],
-  lager:info("Service: ~p", [Service]),
-  lager:info("Version: ~p", [Version]),
-  lager:info("Rest: ~p", [URL]).
-
+  lager:info("Rest: ~p", [URL]),
+  {Name, Version}.
